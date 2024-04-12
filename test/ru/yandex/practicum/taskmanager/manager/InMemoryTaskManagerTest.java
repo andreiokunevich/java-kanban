2b055351f80edf5e1355a9bf9f69de848c53820c
andrei_ok <andreiokunevich@yandex.ru>
2024-04-12 20:16:53 +0300

    Добавлен тест продолжительности и дат для эпика

--- test/ru/yandex/practicum/taskmanager/manager/InMemoryTaskManagerTest.java
+++ test/ru/yandex/practicum/taskmanager/manager/InMemoryTaskManagerTest.java
@@ -99,4 +99,42 @@ class InMemoryTaskManagerTest extends TaskManagerTest<InMemoryTaskManager> {
 
         assertEquals(2, manager.getSubTaskById(subTaskId).getId(), "Произошло изменение ID подзадачи.");
     }
+
+    @Test
+    void testUpdateOfEpicsDurationAndTimes() {
+        int idEpic1 = manager.createEpic(new Epic("Epic_1", "Epic_1", 0, Status.NEW,
+                LocalDateTime.of(2024, 6, 9, 10, 45),
+                LocalDateTime.of(2024, 7, 10, 22, 30), Duration.ofMinutes(20)));
+        int idSubtask1Epic1 = manager.createSubTask(new SubTask("Subtask_1_Of_Epic_1", "Subtask_1_Of_Epic_1",
+                0, Status.NEW, idEpic1, LocalDateTime.of(2024, 4, 18, 10, 45), Duration.ofMinutes(30)));
+
+        assertEquals(30, manager.getEpicById(idEpic1).getDuration().toMinutes(),
+                "Время продолжительности эпика рассчитано неверно.");
+        assertEquals(manager.getEpicById(idEpic1).getStartTime(), manager.getSubTaskById(idSubtask1Epic1).getStartTime(),
+                "Дата и время старта эпика неверные.");
+        assertEquals(manager.getEpicById(idEpic1).getEndTime(), manager.getSubTaskById(idSubtask1Epic1).getEndTime(),
+                "Дата и время завершения эпика неверные.");
+
+        int idSubtask2Epic1 = manager.createSubTask(new SubTask("Subtask_2_Of_Epic_1", "Subtask_2_Of_Epic_1",
+                0, Status.NEW, idEpic1, LocalDateTime.of(2024, 4, 13, 11, 12), Duration.ofMinutes(60)));
+        int idSubtask3Epic1 = manager.createSubTask(new SubTask("Subtask_3_Of_Epic_1", "Subtask_3_Of_Epic_1",
+                0, Status.NEW, idEpic1, LocalDateTime.of(2024, 4, 15, 17, 15), Duration.ofMinutes(10)));
+
+        assertEquals(100, manager.getEpicById(idEpic1).getDuration().toMinutes(),
+                "Время продолжительности эпика рассчитано неверно.");
+        assertEquals(manager.getEpicById(idEpic1).getStartTime(), manager.getSubTaskById(idSubtask2Epic1).getStartTime(),
+                "Дата и время старта эпика неверные.");
+        assertEquals(manager.getEpicById(idEpic1).getEndTime(), manager.getSubTaskById(idSubtask1Epic1).getEndTime(),
+                "Дата и время завершения эпика неверные.");
+
+        int idSubtask4Epic1 = manager.createSubTask(new SubTask("Subtask_3_Of_Epic_1", "Subtask_3_Of_Epic_1",
+                0, Status.NEW, idEpic1, null, Duration.ofMinutes(10)));
+
+        assertEquals(100, manager.getEpicById(idEpic1).getDuration().toMinutes(),
+                "Время продолжительности эпика изменилось после добавления подзадачи без времени начала.");
+        assertEquals(manager.getEpicById(idEpic1).getStartTime(), manager.getSubTaskById(idSubtask2Epic1).getStartTime(),
+                "Дата и время старта эпика изменились после добавления подзадачи без времени начала.");
+        assertEquals(manager.getEpicById(idEpic1).getEndTime(), manager.getSubTaskById(idSubtask1Epic1).getEndTime(),
+                "Дата и время завершения эпика изменились после добавления подзадачи без времени начала.");
+    }
 }
